c3fb66d10d476644012911e4a92afe6ff87f0870
angelaxie2015 <angelaxie2015@gmail.com>
2021-02-06 15:10:32 -0500

    style navbar

--- src/Navigation/Navigation.js
+++ src/Navigation/Navigation.js
@@ -3,51 +3,46 @@ import "./Navigation.css";
 
 // import { Route, BrowserRouter, Link } from "react-router-dom";
 import {AppBar, Toolbar, IconButton, Typography, Button, Avatar } from '@material-ui/core';
-import {Navbar, Nav} from 'react-bootstrap';
+import {Navbar, Nav, Form, FormControl} from 'react-bootstrap';
 import Register from "../Register/Register";
 
 function Navigation() {
 	
-	
 	return (
 		<div className="navigation">
 
-			<AppBar position="static" className="navbar">
-			  <Toolbar>
-
-			  	<Navbar.Brand href="/">
+			<Navbar className="navbar">
+			    <Navbar.Brand href="/" className="nav-img">
 			    	<img src="../../NewBeeLogo.png" alt="bee image" className="nav-img"></img>
 			 	</Navbar.Brand>
+			    <Nav className="mr-auto">
+			      <Nav.Link href="register">Register</Nav.Link>
+			      <Nav.Link href="feed">Browse</Nav.Link>
+			    </Nav>
 
-				<form>
-					<label>
-						Username:
-						<input type="text" name="username" />
-						password:
-						<input type="text" name="password" />
-					</label>
-					<input type="submit" value="Submit" />
-				</form>
-
-			    <Nav.Link href="">
-			    	<span className="nav-link" color="inherit">LogIn</span>
-			    </Nav.Link>
-
-			    <Nav.Link href="register">
-			    	<span className="nav-link" color="inherit">Register</span>
-			    </Nav.Link>
-
-			    <Nav.Link href="feed">
-			    	<span className="nav-link" color="inherit">Feeds</span>
-			    </Nav.Link>
-
-
-			    <div className="user">
-				    <Avatar />
-				    <h4 className="nav-username">UserName</h4>
+			    <div className="nav-right-hand">
+				    <form className="nav-form">
+						<label className="nav-input">
+							<p>Username: </p>
+							<input className="nav-box" type="text" name="username" />
+							password:
+							<input className="nav-box" type="text" name="password" />
+							<input className="nav-submit" type="submit" value="Submit" />
+						</label>
+					</form>
 				</div>
-			  </Toolbar>
-			</AppBar>
+
+				{/* Depending on log in, toggle in between login ask and username display 
+
+				<div className="nav-right-hand">
+					<div className="user">
+					    <Avatar />
+					    <h4 className="nav-username">UserName</h4>
+					</div>
+				</div>
+
+				*/}
+			</Navbar>
 
 		</div>
 	);
