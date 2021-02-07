0893905065dabd680e150f9bbd3f56676cb15513
angelaxie2015 <angelaxie2015@gmail.com>
2021-02-07 01:49:40 -0500

    style feed page and chatbot

--- src/App.js
+++ src/App.js
@@ -5,10 +5,6 @@ import Register from "./Register/Register.js";
 import Home from "./Home/home.js";
 import Feed from "./Feed/feed.js";
 
-import Chatbot from 'react-chatbot-kit'
-import ActionProvider from './ChatBot/actionprovider';
-import MessageParser from "./ChatBot/messageparser";
-import config from './ChatBot/config';
 
 
 function App() {
@@ -23,10 +19,6 @@ function App() {
             <Redirect to="/" />
            </Route>
         </Switch>
-
-        <div className="chatbot">
-          <Chatbot config={config} actionProvider={ActionProvider} messageParser={MessageParser} />
-        </div>
       </BrowserRouter>
 
 
