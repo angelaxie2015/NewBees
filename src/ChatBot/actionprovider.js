b0926097c30bbeac727407f2237fcee6be81f85c
angelaxie2015 <angelaxie2015@gmail.com>
2021-02-06 21:03:53 -0500

    chatbot

--- src/ChatBot/actionprovider.js
+++ src/ChatBot/actionprovider.js
@@ -1,4 +1,43 @@
+import { InstantSearch, Hits, Highlight, SearchBox } from 'react-instantsearch-dom';
+
+const Hit = ({hit}) => {
+  <div className="hit">
+    <div className="hitImage">
+      <img src={hit.image}/>
+    </div>
+      <div className = "hitName">
+        <Highlight attribute = "name" hit = {hit}/>
+      </div>
+  </div>
+}
+
+const Content = () => {
+  <div className = "content">
+  	<Hits hitComponent = {Hit} />
+  </div>
+}
+
 class ActionProvider {
+
+	search(m){
+		console.log("I'm here in the search engine");
+		<InstantSearch
+		    appId="latency"
+		    apiKey="3d9875e51fbd20c7754e65422f7ce5e1"
+		    indexName="bestbuy"
+		 >
+
+		<header>
+          <SearchBox translations = {{placeholder:'Search Box'}}/>
+      </header>
+
+		 <main>
+		  <Content/>
+		</main>
+
+
+		</InstantSearch>
+	}
 
   constructor(createChatBotMessage, setStateFunc, createClientMessage) {
     this.createChatBotMessage = createChatBotMessage;
